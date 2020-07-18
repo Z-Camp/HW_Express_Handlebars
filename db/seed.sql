cbd02beae66d8c70454dbd01acb45efd7d62caa2
Romo <60673021+Z-Camp@users.noreply.github.com>
2020-07-18 18:21:23 -0400

    repo init

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (id, burger_name, devoured, date)
+VALUES
+(null, "Cheese Burger", FALSE, null),
+(null, "Bacon Burger", FALSE, null),
+(null, "Quarter Pounder Burger", FALSE, null),
+(null, "Half-Pounder Burger", FALSE, null),
+(null, "Triple Decker Burger", FALSE, null),
+(null, "Impossible Burger", FALSE, null);
